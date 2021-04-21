53063a0574f0c05413b599e92f25e3fbffb9bcda
Gurpreet Singh <gurpreetsingh@192.168.1.2>
2021-04-21 10:58:13 +0530

    added const icon map

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -3,15 +3,32 @@ import {
     FaGithub,
     FaInstagram,
     FaLinkedin,
-    FaHome
+    FaHome,
+    // skill icons
+    FaReact,
+    FaAngular,
+    FaSass,
+    FaDatabase
 } from 'react-icons/fa'
+
+import {
+    SiCplusplus,
+    SiGraphql,
+    SiLinux,
+    SiJava,
+    SiJavascript
+} from 'react-icons/si'
+
+import {
+    AiOutlineConsoleSql
+} from 'react-icons/ai'
 
 import {
     MdEmail,
     MdPhone
 } from 'react-icons/md'
 
-export const ICON_COMPONENT_MAP = {
+export const SOCIAL_ICON_COMP_MAP = {
     email: <MdEmail/>,
     phone: <MdPhone/>,
     site: <FaHome/>,
@@ -19,4 +36,44 @@ export const ICON_COMPONENT_MAP = {
     linkedin: <FaLinkedin/>,
     github: <FaGithub/>,
     instagram: <FaInstagram/>
+}
+
+
+export const SKILL_ICON_COMP_MAP = {
+    'reactjs': {
+        icon: <FaReact />,
+        label: 'ReactJS'
+    },
+    'angularjs': {
+        icon: <FaAngular />,
+        label: 'AngularJS'
+    },
+    'c++': {
+        icon: <SiCplusplus />,
+        label: 'C++'
+    },
+    'plsql': {
+        icon: <FaDatabase />,
+        label: 'PL/SQL'
+    },
+    'graphql': {
+        icon: <SiGraphql />,
+        label: 'GraphQL'
+    },
+    'linux': {
+        icon: <SiLinux />,
+        label: 'Linux'
+    },
+    'java': {
+        icon: <SiJava />,
+        label: 'Java'
+    },
+    'javascript': {
+        icon: <SiJavascript />,
+        label: 'Javascript'
+    },
+    'sass': {
+        icon: <FaSass />,
+        label: 'Sass'
+    }
 }
