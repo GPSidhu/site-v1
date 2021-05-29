178e201710cd06e3d9157db187948f4c321c6b85
Gurpreet Singh <gurpreetsingh@192.168.1.5>
2021-05-29 11:28:26 +0530

    Build failure fixes

--- src/components/content/about/index.js
+++ src/components/content/about/index.js
@@ -1,132 +1,123 @@
-import React from 'react'
-import styled from 'styled-components'
-import {
-    SectionContainer,
-    SectionWrapper,
-} from '../../common'
+import React from "react";
+import styled from "styled-components";
+import { SectionContainer, SectionWrapper } from "../../common";
 
 export const Row = styled.div`
-    display: grid;
-    align-items: flex-start;
-    grid-template-areas: 'pic info';
-    grid-template-columns: 2fr 3fr;
+	display: grid;
+	align-items: flex-start;
+	grid-template-areas: "pic info";
+	grid-template-columns: 2fr 3fr;
 
-    @media screen and (max-width: 768px) {
-        grid-template-areas: 'pic' 'info';
-        grid-template-columns: auto;
-        grid-auto-rows: auto 3fr;
-    }
-`
+	@media screen and (max-width: 768px) {
+		grid-template-areas: "pic" "info";
+		grid-template-columns: auto;
+		grid-auto-rows: auto 3fr;
+	}
+`;
 
 export const Avatar = styled.div`
-    margin-bottom: 15px;
-    padding: 0 15px;
-    grid-area: pic;
-    display: flex;
-    justify-content: center;
-`
+	margin-bottom: 15px;
+	padding: 0 15px;
+	grid-area: pic;
+	display: flex;
+	justify-content: center;
+`;
 
 export const Info = styled.div`
-    margin-bottom: 15px;
-    padding: 0 15px;
-    grid-area: info;
-    display: flex;
-    justify-content: center;
-`
+	margin-bottom: 15px;
+	padding: 0 15px;
+	grid-area: info;
+	display: flex;
+	justify-content: center;
+`;
 
 export const TextWrapper = styled.div`
-    max-width: 540px;
-    padding-top: 0;
-    padding-bottom: 60px;
-`
+	max-width: 540px;
+	padding-top: 0;
+	padding-bottom: 60px;
+`;
 
 export const TopLine = styled.p`
-    color: ${({theme}) => theme.main.textPrimary};
-    font-size: 16px;
-    line-height: 16px;
-    font-weight: 700;
-    letter-spacing: 1.4px;
-    text-transform: uppercase;
-    margin-bottom: 16px;
-`
+	color: ${({ theme }) => theme.main.textPrimary};
+	font-size: 16px;
+	line-height: 16px;
+	font-weight: 700;
+	letter-spacing: 1.4px;
+	text-transform: uppercase;
+	margin-bottom: 16px;
+`;
 
 export const Heading = styled.h1`
     margin-bottom: 24px;
     font-size: 48px;
     line-height: 1.1;
     font-weight 600;
-    color: ${({theme}) => theme.main.textSecondary};
+    color: ${({ theme }) => theme.main.textSecondary};
 
     @media screen and (max-width: 480px) {
         font-size: 32px;
     }
-`
+`;
 
 export const Subtitle = styled.p`
-    max-width: 440px;
-    margin-bottom: 35px;
-    font-size: 18px;
-    line-height: 24px;
-    color: ${({theme}) => theme.main.textPrimary}
-`
+	max-width: 440px;
+	margin-bottom: 35px;
+	font-size: 18px;
+	line-height: 24px;
+	color: ${({ theme }) => theme.main.textPrimary};
+`;
 
 export const BtnWrap = styled.div`
-    display: flex;
-    justify-content: flex-start;
-`
+	display: flex;
+	justify-content: flex-start;
+`;
 
 export const ImgWrap = styled.div`
-    max-width: 555px;
-    height: 100%;
-    display: flex;
-    justify-content: flex-end;
-    @media screen and (max-width: 780px) {
-        justify-content: center;
-    }
-`
+	max-width: 555px;
+	height: 100%;
+	display: flex;
+	justify-content: flex-end;
+
+	@media screen and (max-width: 780px) {
+		justify-content: center;
+	}
+`;
 
 export const Img = styled.img`
-    width: 50%;
-    border: 2px solid ${({theme}) => theme.main.borderPrimary};
-    border-radius: 50%;
-    margin: 0 0 10px 0;
-    padding-right: 0;
-    @media screen and (max-width: 480px) {
-       width: 70%;
-    }
-`
+	width: 50%;
+	border: 2px solid ${({ theme }) => theme.main.borderPrimary};
+	border-radius: 50%;
+	margin: 0 0 10px 0;
+	padding-right: 0;
 
-function About({
-    id,
-    topLine,
-    headline,
-    description,
-    img,
-    alt,
-    seq
- }) {
-    return (
-        <>
-            <SectionContainer seq={seq} id={id} style={{height: '960px'}}>
-                <SectionWrapper>
-                    <Row>
-                        <Avatar>
-                            <ImgWrap>
-                                <Img src={`/images/${img}`} alt={alt}></Img>
-                            </ImgWrap>
-                        </Avatar>
-                        <Info>
-                            <TextWrapper>
-                                <TopLine>{topLine}</TopLine>
-                                <Heading>{headline}</Heading>
-                                <Subtitle>{description}</Subtitle>
-                            </TextWrapper>
-                        </Info>
-                    </Row>
-                </SectionWrapper>
-            </SectionContainer>
-        </>
-    )
+	@media screen and (max-width: 480px) {
+		width: 70%;
+	}
+`;
+
+function About({ id, topLine, headline, description, img, alt, seq }) {
+	return (
+		<>
+			<SectionContainer seq={seq} id={id} style={{ height: "960px" }}>
+				<SectionWrapper>
+					<Row>
+						<Avatar>
+							<ImgWrap>
+								<Img src={`/images/${img}`} alt={alt}></Img>
+							</ImgWrap>
+						</Avatar>
+						<Info>
+							<TextWrapper>
+								<TopLine>{topLine}</TopLine>
+								<Heading>{headline}</Heading>
+								<Subtitle>{description}</Subtitle>
+							</TextWrapper>
+						</Info>
+					</Row>
+				</SectionWrapper>
+			</SectionContainer>
+		</>
+	);
 }
 
-export default About
+export default About;
--- src/components/content/cover/index.js
+++ src/components/content/cover/index.js
@@ -1,52 +1,38 @@
-import React  from 'react'
-// { useEffect, useState } 
+import React from "react";
 
 import {
-    CoverContainer,
-    CoverBg,
-    ImageBg,
-    Content,
-    Quote,
-    Author,
-    BtnWrapper,
-    ArrowDownward
-} from './elements'
-import Button from '../../common/Button';
+	CoverContainer,
+	CoverBg,
+	ImageBg,
+	Content,
+	Quote,
+	Author,
+} from "./elements";
 
-const Cover = ({backgroundColor, backgroundImage, quotes}) => {
-    // const [scrollTop, setScrollTop] = useState(false)
-
-    // const updateCoverBg = () => {
-    //     let top = window.scrollY || 0;
-    //     setScrollTop(top);
-    // }
-
-    // useEffect(() => {
-    //     window.addEventListener('scroll', updateCoverBg)
-    // }, [])
-
-    return (
-        <div>
-            <CoverContainer>
-                <CoverBg>
-                    {backgroundImage && <ImageBg></ImageBg>}
-                </CoverBg>
-                <Content>
-                    {quotes && quotes.map((q, i) => (
-                        <div key={`q_${i}`} >
-                            <Quote size={q.size}>{q.text}</Quote>
-                            <Author size={q.size}><span>{`- ${q.author}`}</span></Author>
-                        </div>
-                    ))}
-                </Content>
-                {/* <BtnWrapper>
+const Cover = ({ backgroundImage, quotes }) => {
+	return (
+		<div>
+			<CoverContainer>
+				<CoverBg>{backgroundImage && <ImageBg></ImageBg>}</CoverBg>
+				<Content>
+					{quotes &&
+						quotes.map((q, i) => (
+							<div key={`q_${i}`}>
+								<Quote size={q.size}>{q.text}</Quote>
+								<Author size={q.size}>
+									<span>{`- ${q.author}`}</span>
+								</Author>
+							</div>
+						))}
+				</Content>
+				{/* <BtnWrapper>
                     <Button >
                         <ArrowDownward />
                     </Button>
                 </BtnWrapper> */}
-            </CoverContainer>
-        </div>
-    )
-}
+			</CoverContainer>
+		</div>
+	);
+};
 
-export default Cover
+export default Cover;
