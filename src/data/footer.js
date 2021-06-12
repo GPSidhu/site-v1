92647d2d576c50966e9735f03aef9876f53e70df
Gurpreet Singh <gurpreetsingh@192.168.1.3>
2021-06-12 21:50:23 +0530

    Resume tooltip and sidebar updates

--- src/data/footer.js
+++ src/data/footer.js
@@ -1,18 +1,30 @@
 const data = {
-    rights: 'Designed and built by Gurpreet Singh. All rights reserved.',
-    links: [
-        // to do: create link pages
-        // {id: 'about', to: '/about', label: 'About'},
-        // {id: 'license', to: '/license', label: 'License'},
-        // {id: 'privacy', to: '/privacy', label: 'Privacy'}
-    ],
-    contacts: [
-        {id: 'linkedin', label: 'Linkedin', href: '//www.linkedin.com/in/gurpreet-singh-75266446/'},
-        {id: 'facebook', label: 'Facebook', href: '/'},
-        {id: 'instagram', label: 'Instagram', href: '/'},
-        {id: 'github', label: 'Github', href: '//www.github.com/GPSidhu'},
-    ],
-    source: 'https://github.com/GPSidhu/site-v1'
-}
+	rights: "Designed and built by Gurpreet Singh. All rights reserved.",
+	links: [
+		// to do: create link pages
+		// {id: 'about', to: '/about', label: 'About'},
+		// {id: 'license', to: '/license', label: 'License'},
+		// {id: 'privacy', to: '/privacy', label: 'Privacy'}
+	],
+	contacts: [
+		{
+			id: "linkedin",
+			label: "Linkedin",
+			href: "https://www.linkedin.com/in/gurpreet-singh-75266446/",
+		},
+		{
+			id: "facebook",
+			label: "Facebook",
+			href: "https://www.facebook.com/gsinghsidhu/",
+		},
+		// {id: 'instagram', label: 'Instagram', href: '/'},
+		{
+			id: "github",
+			label: "Github",
+			href: "https://www.github.com/GPSidhu",
+		},
+	],
+	source: "https://github.com/GPSidhu/site-v1",
+};
 
-export default data
+export default data;
